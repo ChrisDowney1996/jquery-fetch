791345da71c916345867de593e6777ab448464e4
安逗 <cn80393313@gmail.com>
2019-02-22 11:13:54 +0800

    说明文档更新

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,10 +15,6 @@ module.exports = function(grunt) {
       files: ['Gruntfile.js', 'src/**/*.js']
     },
   })
-
-  // 加载包含 "uglify" 任务的插件。
   grunt.loadNpmTasks('grunt-contrib-uglify')
-
-  // 默认被执行的任务列表。
   grunt.registerTask('default', ['uglify'])
 }
